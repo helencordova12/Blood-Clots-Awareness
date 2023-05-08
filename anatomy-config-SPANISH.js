37a46634f7fa56e55a75249f5eb9dfb1bf7021d9
Margarita Cordova <helencordovar@gmail.com>
2023-05-07 20:44:10 -0700

    Add files via upload

---
+++ anatomy-config-SPANISH.js
@@ -0,0 +1,57 @@
+var basic_config = {
+    "basic_1":{//head
+        "hover": "CEREBRO: Los coágulos sanguíneos en el cerebro pueden causar diversos síntomas, como cara caída, náuseas, fiebre, debilidad en los brazos, dificultad para ver o hablar y confusión repentina.",//info of the popup
+        "url": "https://www.cdc.gov/ncbddd/spanish/dvt/features/trombosisvenosaprofunda.html",//link to any webpage
+        "target": "_blank",// use "_blank", "_self", or "none"
+        "active": true//true/false to show/hide
+    },
+    "basic_2":{//neck
+        "hover":"CUELLO: " + "Algunos signos y síntomas de coágulos sanguíneos en el cuello son hinchazón y molestias en el cuello.",
+        "url": "https://www.cdc.gov/ncbddd/spanish/dvt/features/trombosisvenosaprofunda.html", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_3":{//chest
+        "hover": "PECHO: <br/><br/>"+ "1. CORAZÓN: Algunos signos y síntomas de coágulos sanguíneos en el corazón son dolor en el pecho, dolor en la mandíbula, dolor en el brazo izquierdo, dificultad para respirar, dolor abdominal y náuseas.<br/><br/>"+
+        "2. PULMONES: Algunos signos y síntomas de coágulos de sangre en los pulmones son dolor intenso en el pecho, latidos irregulares del corazón, tos con sangre y dificultad para respirar",
+        "url": "https://salud.nih.gov/recursos-de-salud/nih-noticias-de-salud/coagulos-de-sangre-explicados", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_4":{//abdomen
+        "hover": "ABDOMEN: " + "Algunos signos de coágulos de sangre en el abdomen son dolor abdominal intenso, náuseas, heces con sangre, diarrea y distensión o hinchazón abdominal",
+        "url": "https://www.cdc.gov/ncbddd/spanish/dvt/features/trombosisvenosaprofunda.html", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_5":{//pelvis
+        "hover": "PELVIS: " + "Algunos signos y síntomas de coágulos sanguíneos en la pelvis sonn fevel, escalofríos, náuseas e hinchazónnde las piernas",
+        "url": "https://www.cdc.gov/ncbddd/spanish/dvt/features/trombosisvenosaprofunda.html", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_6":{//arm-rt
+        "hover": "BRAZOS: "+ "Algunos indicios de coágulos sanguíneos en los brazos son hinchazón, enrojecimiento o decoloración de la piel, dolor o sensibilidad y sensación de que la piel está más caliente de lo habitual al tacto",
+        "url": "https://salud.nih.gov/recursos-de-salud/nih-noticias-de-salud/coagulos-de-sangre-explicados", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_7":{//arm-lt
+        "hover":  "BRAZOS: "+ "Algunos indicios de coágulos sanguíneos en los brazos son hinchazón, enrojecimiento o decoloración de la piel, dolor o sensibilidad y sensación de que la piel está más caliente de lo habitual al tacto",
+        "url": "https://salud.nih.gov/recursos-de-salud/nih-noticias-de-salud/coagulos-de-sangre-explicados", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_8":{//leg-rt
+        "hover": "PIERNAS: "+ "Algunos signos y síntomas de coágulos sanguíneos en los brazos son hinchazón en los brazos, enrojecimiento o decoloración de la piel, dolor o sensibilidad en los brazos o sensación de que la piel está más caliente de lo normal al tacto.",
+        "url": "https://salud.nih.gov/recursos-de-salud/nih-noticias-de-salud/coagulos-de-sangre-explicados", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_9":{//leg-lt
+        "hover": "PIERNAS: "+ "Algunos signos y síntomas de coágulos sanguíneos en los brazos son hinchazón en los brazos, enrojecimiento o decoloración de la piel, dolor o sensibilidad en los brazos o sensación de que la piel está más caliente de lo normal al tacto.",
+        "url": "https://salud.nih.gov/recursos-de-salud/nih-noticias-de-salud/coagulos-de-sangre-explicados",
+        "target": "_blank",
+        "active": true
+    }
+};
---
+++ anatomy-config.js
@@ -0,0 +1,57 @@
+var basic_config = {
+    "basic_1":{//head
+        "hover": "BRAIN: Blood clots in the brain can cause various symptoms, such as face drooping, nausea, fever, arm weakness, difficulty seeing or speaking, and sudden confusion.",//info of the popup
+        "url": "https://www.hopkinsmedicine.org/health/conditions-and-diseases/cerebral-venous-sinus-thrombosis",//link to any webpage
+        "target": "_blank",// use "_blank", "_self", or "none"
+        "active": true//true/false to show/hide
+    },
+    "basic_2":{//neck
+        "hover": "NECK: " + "Some signs and symptoms of blood clots in your neck are neck swelling and discomfort.",
+        "url": "https://www.healthline.com/health/how-to-tell-if-you-have-a-blood-clot", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_3":{//chest
+        "hover": "CHEST: <br/><br/>"+ "1. HEART: Some signs and symptoms of blood clots in your heart are chest pain, jaw pain, left arm pain, difficulty breathing, abdominal pain, and nausea.<br/><br/>"+ 
+        "2. LUNGS: Some signs and symptoms of blood clots in your lungs are intense chest pain, irregular heartbeat, coughing up blood, and difficult breathing.",
+        "url": "https://www.stoptheclot.org/spreadtheword/#signs", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_4":{//abdomen
+        "hover": "ABDOMEN: " + "Some signs of blood clots in your abdomen are severe abdominal pain,  nausea, bloody stools, diarrhea, and  bloating or swelling in the abdomen.",
+        "url": "https://www.healthline.com/health/how-to-tell-if-you-have-a-blood-clot", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_5":{//pelvis
+        "hover": "PELVIS: " + "Some signs and symptoms of blood clots in your pelvis are fevel, chills, nausea, andd leg swelling.",
+        "url": "https://columbiasurgery.org/conditions-and-treatments/pelvic-vein-disease", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_6":{//arm-rt
+        "hover": "ARMS: "+ "Some indications of blood clots in the arms include swelling, redness or discoloration of the skin, pain or tenderness, and skin that feels warmer than usual to the touch.",
+        "url": "https://www.stoptheclot.org/spreadtheword/#signs", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_7":{//arm-lt
+        "hover": "ARMS: "+ "Some indications of blood clots in the arms include swelling, redness or discoloration of the skin, pain or tenderness, and skin that feels warmer than usual to the touch.",
+        "url": "https://www.stoptheclot.org/spreadtheword/#signs", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_8":{//leg-rt
+        "hover": "LEGS: "+ "Some signs and symptoms of blood clots in your arms are swelling in your arms, redness or discoloration of the skin, pain or tenderness in the arms, or skin that feels warmer than usual to the touch.",
+        "url": "https://www.stoptheclot.org/spreadtheword/#signs", 
+        "target": "_blank",
+        "active": true
+    },
+    "basic_9":{//leg-lt
+        "hover": "LEGS: " + "Some signs and symptoms of blood clots in your arms are swelling in your arms, redness or discoloration of the skin, pain or tenderness in the arms, or skin that feels warmer than usual to the touch.",
+        "url": "https://www.stoptheclot.org/spreadtheword/#signs",
+        "target": "_blank",
+        "active": true
+    }
+};
